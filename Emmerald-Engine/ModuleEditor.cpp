f37478e69c5b8936e4f58c6de8059c4f175b3171
Xavi Prats <99949891+Xarderos@users.noreply.github.com>
2023-10-06 08:56:05 +0200

    Cleaning Menu

--- Emmerald-Engine/ModuleEditor.cpp
+++ Emmerald-Engine/ModuleEditor.cpp
@@ -45,99 +45,7 @@ void ModuleEditor::DrawEditor()
     ImGui::NewFrame();
 
 
-    if (ImGui::BeginMainMenuBar())
-    {
-        if (ImGui::BeginMenu("File"))
-        {
-            ImGui::Text("Hello world!");
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("Assets"))
-        {
-            ImGui::Text("Hello world!");
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("Objects"))
-        {
-            ImGui::Text("Hello world!");
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("Help"))
-        {
-            if (ImGui::BeginMenu("About"))
-            {
-                ImGui::Text("Emmerald Engine");
-                ImGui::Text(" ");
-                ImGui::Text("An engine made by two students cursing the third year of Design and Development degree at CITM.");
-                ImGui::Text(" ");
-                ImGui::Text("Engine made by: ");
-                ImGui::Text("Xavi Prats Paredes:  ");
-                if (ImGui::MenuItem("   Github: https://github.com/Xarderos"))
-                {
-                   ShellExecute(NULL, "open", "https://github.com/Xarderos", NULL, NULL, SW_SHOW);
-                }
-                ImGui::Text("Joan Giol Pozo: ");
-                if (ImGui::MenuItem("   Github:  https://github.com/GHOSTVCL"))
-                {
-                    ShellExecute(NULL, "open", "https://github.com/GHOSTVCL", NULL, NULL, SW_SHOW);
-                }
-                ImGui::Text(" ");
-                ImGui::Text("Vendor: %s", glGetString(GL_VENDOR));
-                ImGui::Text("Renderer: %s", glGetString(GL_RENDERER));
-                ImGui::Text("GLSL: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
-                ImGui::Text("OpenGL version supported %s", glGetString(GL_VERSION));
-                if (ImGui::MenuItem("   https://www.opengl.org/"))
-                {
-                    ShellExecute(NULL, "open", "https://www.opengl.org/", NULL, NULL, SW_SHOW);
-                }
-                ImGui::Text("Using Glew %s", glewGetString(GLEW_VERSION));
-                if (ImGui::MenuItem("   https://glew.sourceforge.net/"))
-                {
-                    ShellExecute(NULL, "open", "https://glew.sourceforge.net/", NULL, NULL, SW_SHOW);
-                }
-                SDL_version version;
-                SDL_GetVersion(&version);
-                ImGui::Text("Using SDL %i.%i.%i", version.major, version.minor, version.patch);
-                if (ImGui::MenuItem("   https://www.libsdl.org/"))
-                {
-                    ShellExecute(NULL, "open", "https://www.libsdl.org/", NULL, NULL, SW_SHOW);
-                }
-                const char* version_gui = ImGui::GetVersion();
-                ImGui::Text("Using ImGui %s", version_gui);
-                if (ImGui::MenuItem("   https://github.com/ocornut/imgui"))
-                {
-                    ShellExecute(NULL, "open", "https://github.com/ocornut/imgui", NULL, NULL, SW_SHOW);
-                }
-                ImGui::Text("");
-                ImGui::Text("");
-                ImGui::Text("MIT License");
-                ImGui::Text("");
-                ImGui::Text("Copyright (c) 2023 GHOSTVCL");
-                ImGui::Text("");
-                ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy");
-                ImGui::Text("of this software and associated documentation files (the \"Software\"), to deal");
-                ImGui::Text("in the Software without restriction, including without limitation the rights");
-                ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell");
-                ImGui::Text("copies of the Software, and to permit persons to whom the Software is");
-                ImGui::Text("furnished to do so, subject to the following conditions:");
-                ImGui::Text("The above copyright notice and this permission notice shall be included in all");
-                ImGui::Text("copies or substantial portions of the Software.");
-                ImGui::Text("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
-                ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
-                ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE");
-                ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
-                ImGui::Text("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
-                ImGui::Text("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
-                ImGui::Text("SOFTWARE.");
-                
-            
-
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenu();
-        }
-        ImGui::EndMainMenuBar();
-    }
+    DisplayMainMenuBar();
 
     if (ImGui::Begin("Configuration"))
     {
@@ -150,6 +58,10 @@ void ModuleEditor::DrawEditor()
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
+
+
+
+
 
 bool ModuleEditor::CleanUp()
 {
@@ -177,5 +89,108 @@ void ModuleEditor::AddFPS(const float aFPS)
             }
         }
         mFPSLog[mFPSLog.capacity() - 1] = aFPS;
+    }
+}
+
+
+void ModuleEditor::DisplayHelp()
+{
+    if (ImGui::BeginMenu("Help"))
+    {
+        if (ImGui::BeginMenu("About"))
+        {
+            ImGui::Text("Emmerald Engine");
+            ImGui::Text(" ");
+            ImGui::Text("An engine made by two students cursing the third year of Design and Development degree at CITM.");
+            ImGui::Text(" ");
+            ImGui::Text("Engine made by: ");
+            ImGui::Text("Xavi Prats Paredes:  ");
+            if (ImGui::MenuItem("   Github: https://github.com/Xarderos"))
+            {
+                ShellExecute(NULL, "open", "https://github.com/Xarderos", NULL, NULL, SW_SHOW);
+            }
+            ImGui::Text("Joan Giol Pozo: ");
+            if (ImGui::MenuItem("   Github:  https://github.com/GHOSTVCL"))
+            {
+                ShellExecute(NULL, "open", "https://github.com/GHOSTVCL", NULL, NULL, SW_SHOW);
+            }
+            ImGui::Text(" ");
+            ImGui::Text("Vendor: %s", glGetString(GL_VENDOR));
+            ImGui::Text("Renderer: %s", glGetString(GL_RENDERER));
+            ImGui::Text("GLSL: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+            ImGui::Text("OpenGL version supported %s", glGetString(GL_VERSION));
+            if (ImGui::MenuItem("   https://www.opengl.org/"))
+            {
+                ShellExecute(NULL, "open", "https://www.opengl.org/", NULL, NULL, SW_SHOW);
+            }
+            ImGui::Text("Using Glew %s", glewGetString(GLEW_VERSION));
+            if (ImGui::MenuItem("   https://glew.sourceforge.net/"))
+            {
+                ShellExecute(NULL, "open", "https://glew.sourceforge.net/", NULL, NULL, SW_SHOW);
+            }
+            SDL_version version;
+            SDL_GetVersion(&version);
+            ImGui::Text("Using SDL %i.%i.%i", version.major, version.minor, version.patch);
+            if (ImGui::MenuItem("   https://www.libsdl.org/"))
+            {
+                ShellExecute(NULL, "open", "https://www.libsdl.org/", NULL, NULL, SW_SHOW);
+            }
+            const char* version_gui = ImGui::GetVersion();
+            ImGui::Text("Using ImGui %s", version_gui);
+            if (ImGui::MenuItem("   https://github.com/ocornut/imgui"))
+            {
+                ShellExecute(NULL, "open", "https://github.com/ocornut/imgui", NULL, NULL, SW_SHOW);
+            }
+            ImGui::Text("");
+            ImGui::Text("");
+            ImGui::Text("MIT License");
+            ImGui::Text("");
+            ImGui::Text("Copyright (c) 2023 GHOSTVCL");
+            ImGui::Text("");
+            ImGui::Text("Permission is hereby granted, free of charge, to any person obtaining a copy");
+            ImGui::Text("of this software and associated documentation files (the \"Software\"), to deal");
+            ImGui::Text("in the Software without restriction, including without limitation the rights");
+            ImGui::Text("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell");
+            ImGui::Text("copies of the Software, and to permit persons to whom the Software is");
+            ImGui::Text("furnished to do so, subject to the following conditions:");
+            ImGui::Text("The above copyright notice and this permission notice shall be included in all");
+            ImGui::Text("copies or substantial portions of the Software.");
+            ImGui::Text("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
+            ImGui::Text("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+            ImGui::Text("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE");
+            ImGui::Text("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
+            ImGui::Text("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
+            ImGui::Text("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+            ImGui::Text("SOFTWARE.");
+
+
+
+            ImGui::EndMenu();
+        }
+        ImGui::EndMenu();
+    }
+}
+void ModuleEditor::DisplayMainMenuBar()
+{
+    if (ImGui::BeginMainMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            ImGui::Text("Hello world!");
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Assets"))
+        {
+            ImGui::Text("Hello world!");
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Objects"))
+        {
+            ImGui::Text("Hello world!");
+            ImGui::EndMenu();
+        }
+
+        DisplayHelp();
+        ImGui::EndMainMenuBar();
     }
 }
