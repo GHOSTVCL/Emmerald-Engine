521b19d5cf839842d81fca3fddb309781f5fab0a
Xarderos <99949891+Xarderos@users.noreply.github.com>
2023-10-14 12:51:09 +0200

    Assimp fixed

--- Emmerald-Engine/ModuleImporter.cpp
+++ Emmerald-Engine/ModuleImporter.cpp
@@ -1,14 +1,9 @@
 #include "ModuleImporter.h"
-#include "Assimp/include/assimp/cimport.h"
-#include "Assimp/include/assimp/scene.h"
-#include "Assimp/include/assimp/postprocess.h"
+#include "Assimp/include/cimport.h"
+#include "Assimp/include/scene.h"
+#include "Assimp/include/postprocess.h"
 
-
-#ifdef _DEBUG
-#pragma comment (lib, "Assimp/libx86/Debug/assimp-vc142-mtd.lib")
-#else
-#pragma comment (lib, "Assimp/libx86/Release/assimp-vc142-mtd.lib") 
-#endif // _DEBUG
+#pragma comment (lib, "Assimp/libx86/assimp.lib")
 
 ModuleImporter::ModuleImporter(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
