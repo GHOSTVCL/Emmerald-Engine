11ff6e3cee0dc01e1206231c765286088fe7a614
Xarderos <99949891+Xarderos@users.noreply.github.com>
2023-12-09 03:52:19 +0100

    Genialidad infinita

--- Emmerald-Engine/ModuleCamera3D.cpp
+++ Emmerald-Engine/ModuleCamera3D.cpp
@@ -59,7 +59,11 @@ update_status ModuleCamera3D::Update(float dt)
 	if (App->input->GetKey(SDL_SCANCODE_LALT) == KEY_REPEAT && App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
 	{
 		if (App->scene->selectedGO != nullptr) {
-			float3 target = App->scene->selectedGO->GetComponent<CompTransform>()->position;
+
+			float3 target = FindTargetRotation(App->scene->selectedGO);
+
+	
+
 			sceneCam->LookAt(target);
 
 			float TargetDist = sceneCam->FrustumCam.pos.Distance(target);
@@ -81,99 +85,7 @@ update_status ModuleCamera3D::Update(float dt)
 
 	if (wheel != 0) sceneCam->FrustumCam.pos += sceneCam->FrustumCam.front * 10 * -wheel;
 
-	// Implement a debug camera with keys and mouse
-	// Now we can make this movememnt frame rate independant!
-
-	/*vec3 newPos(0, 0, 0);
-	float speed = 3.0f * dt;
-
-	Position += newPos;
-	Reference += newPos;
-
-	if ((App->input->GetMouseZ() != 0))
-	{
-		newPos -= Z * speed * App->input->GetMouseZ() * 150;
-
-		Position += newPos;
-		Reference += newPos;
-	}
-
-	if(App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-	{
-		click = true;
-		int dx = -App->input->GetMouseXMotion();
-		int dy = -App->input->GetMouseYMotion();
-
-		float Sensitivity = 0.25f;
-
-		Position -= Reference;
-
-		if(dx != 0)
-		{
-			float DeltaX = (float)dx * Sensitivity;
-
-			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-		}
-
-		if(dy != 0)
-		{
-			float DeltaY = (float)dy * Sensitivity;
-
-			Y = rotate(Y, DeltaY, X);
-			Z = rotate(Z, DeltaY, X);
-
-			if(Y.y < 0.0f)
-			{
-				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
-				Y = cross(Z, X);
-			}
-		}
-
-		Position = Reference + Z * length(Position);
-	}*/
-
-
-
-	/*if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
-	{
-		click = true;
-		int dx = -App->input->GetMouseXMotion();
-		int dy = -App->input->GetMouseYMotion();
-
-		float Sensitivity = 0.25f;
-
-		Position -= Reference;
-
-		if (dx != 0)
-		{
-			float DeltaX = (float)dx * Sensitivity;
-
-			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
-		}
-
-		if (dy != 0)
-		{
-			float DeltaY = (float)dy * Sensitivity;
-
-			Y = rotate(Y, DeltaY, X);
-			Z = rotate(Z, DeltaY, X);
-
-			if (Y.y < 0.0f)
-			{
-				Z = vec3(0.0f, Z.y > 0.0f ? 1.0f : -1.0f, 0.0f);
-				Y = cross(Z, X);
-			}
-		}
-
-		Position = Reference + Z * length(Position);
-	}*/
-
-	// Recalculate matrix -------------
-	//CalculateViewMatrix();
+	
 
 	return UPDATE_CONTINUE;
 }
@@ -209,52 +121,16 @@ void ModuleCamera3D::Rotation()
 	sceneCam->FrustumCam.SetWorldMatrix(matrix.Float3x4Part());
 }
 
-// -----------------------------------------------------------------
-//void ModuleCamera3D::Look(const vec3 &Position, const vec3 &Reference, bool RotateAroundReference)
-//{
-//	this->Position = Position;
-//	this->Reference = Reference;
-//
-//	Z = normalize(Position - Reference);
-//	X = normalize(cross(vec3(0.0f, 1.0f, 0.0f), Z));
-//	Y = cross(Z, X);	
-//
-//	if(!RotateAroundReference)
-//	{
-//		this->Reference = this->Position;
-//		this->Position += Z * 0.05f;
-//	}
-//
-//	CalculateViewMatrix();
-//}
-//
-//// -----------------------------------------------------------------
-//void ModuleCamera3D::LookAt( const vec3 &Spot)
-//{
-//	Reference = Spot;
-//
-//	Z = normalize(Position - Reference);
-//	X = normalize(cross(vec3(0.0f, 1.0f, 0.0f), Z));
-//	Y = cross(Z, X);
-//
-//	CalculateViewMatrix();
-//}
-//
-//
-//// -----------------------------------------------------------------
-//void ModuleCamera3D::Move(const vec3 &Movement)
-//{
-//	Position += Movement;
-//	Reference += Movement;
-//
-//	CalculateViewMatrix();
-//}
-//
-//// -----------------------------------------------------------------
-//
-//// -----------------------------------------------------------------
-//void ModuleCamera3D::CalculateViewMatrix()
-//{
-//	ViewMatrix = mat4x4(X.x, Y.x, Z.x, 0.0f, X.y, Y.y, Z.y, 0.0f, X.z, Y.z, Z.z, 0.0f, -dot(X, Position), -dot(Y, Position), -dot(Z, Position), 1.0f);
-//	ViewMatrixInverse = inverse(ViewMatrix);
-//}
+float3 ModuleCamera3D::FindTargetRotation(GameObject* tempGO)
+{
+	float3 target;
+
+	target = tempGO->GetComponent<CompTransform>()->position;
+	if (tempGO->parent != nullptr && tempGO->parent->name != "root") {
+		target = FindTargetRotation(tempGO->parent) + target;
+	}
+	return target;
+}
+
+
+
